28daf2a541760f884598c4dc0850eb8f99032e8f
Eric Steele <esteele@plone.org>
2011-05-21 02:24:07 +0000

    Initial import.
    svn path=/plone.phonehome/trunk/; revision=49900

---
+++ plone/phonehome/interfaces.py
@@ -0,0 +1,18 @@
+from zope.exceptions import UserError
+
+# CHECK_URL = 'http://localhost:9999/check?uid=%s&hash=%s'
+# UPDATE_URL = 'http://localhost:9999/update'
+
+CHECK_URL = 'http://plonephonehome.appspot.com/check?uid=%s&hash=%s'
+UPDATE_URL = 'http://plonephonehome.appspot.com/update'
+
+
+CALL_TIMEOUT = 5
+
+OKCODE = 'OK' # everything is ok
+UPDATECODE = 'UPDATE' # hash has changes, requires to send full info
+FAILEDCODE = 'FAILED' # check-in procedure failed
+
+
+class ConnectionProblem(UserError):
+    pass
